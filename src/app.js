f31e9e1e75faa4897902a58ede3e54326c9984aa
kashi4444 <kashishagarwal594@gmail.com>
2025-07-26 20:16:20 +0530

    Updated app.js with new changes

--- src/app.js
+++ src/app.js
@@ -4,16 +4,10 @@ const express = require('express');
 //new app created
 const app = express();
 
-
-app.use("/hello", (req, res)=>{
-    res.send("Hello Hello Hello!")
-})
-app.use("/test", (req, res)=>{
-    res.send("Hello from the server!")
-})
-app.use("/", (req, res)=>{
-    res.send("Hello from the dashboard!")
-})
+app.get("/user/:userId/:name/:password", (req, res)=>{
+    console.log(req.params);
+    res.send("Got it");
+});
 //app is listening or running on port 3000
 app.listen(7777, ()=>{
     console.log("Server is successfully listening on port 7777..");
