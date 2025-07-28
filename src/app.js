6b4918679b27df57ccc3cd2b0c51baae728f64e8
kashi4444 <kashishagarwal594@gmail.com>
2025-07-28 17:27:25 +0530

    Data sanitisatio

--- src/app.js
+++ src/app.js
@@ -1,23 +1,75 @@
 //create server
 const express = require('express');
 const connectDB = require("./config/database");
-const User = require("./models/user");
+
 //create app
 const app = express();
+const User = require("./models/user");
 
+app.use(express.json());
+
+//get a user data by email
+app.get("/user", async(req,res)=>{
+    const userEmail = req.body.email;
+    try{
+        const user = await User.find({email: userEmail});
+        res.send(user);
+    }catch(err){
+        res.status(400).send("user not found");
+    }
+    
+})
+//to get all the users
+app.get("/feed",async(req, res)=>{
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }catch(err){
+        res.status(400).send("user not found");
+    }
+})
+
+app.delete("/delete", async(req,res)=>{
+    const userId = req.body.userId;
+    try{
+        await User.findByIdAndDelete(userId);
+        res.send("User Deleted Successfully");
+    }catch(err){
+        res.status(400).send("user not Deleted");
+    }
+})
+//update
+app.patch("/user/:userId", async(req,res)=>{
+    const userId = req.params.userId;
+    const data = req.body;
+
+    try{
+        const ALLOWED_UPDATES = ["photoUrl","about","gender","age","skills"];
+        const isUpdateAllowed = Object.keys(data).every((k)=> ALLOWED_UPDATES.includes(k));
+        if(!isUpdateAllowed){
+            res.status(400).send("Update Not Allowed");
+        }
+
+        if(data?.skills.length > 10){
+            throw new Error("Skills cannot be more than 10");
+        }
+
+        await User.findByIdAndUpdate(userId, data, {
+            returnDocument:"after",
+            runValidators:true
+        });
+        res.send("Details updated successfully");
+    }catch(err){
+        res.status(400).send("user not updated"+ err.message);
+    }
+})
 app.post("/signup", async(req,res)=>{
     //create instance 
-    const user = new User({
-        firstName: "Sunil",
-        lastName :"Kohli",
-        email:"Virat@ag.com",
-        password:"Virat@123",
-        _id: "123"
-    })
+    const user = new User(req.body);
     try{
-        user.save();
+        await user.save();
         res.send("User Added Successfully");
-    }catch(err){
+    }catch(err){ 
         res.status(400).send("Error in saving the message: "+ err.message);
     }
     
--- src/models/user.js
+++ src/models/user.js
@@ -1,23 +1,62 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 const userSchema = new mongoose.Schema({
     firstName:{
         type : String,
+        required: true,
+        minLength : 4,
+        maxLength : 50
     },
     lastName :{
         type: String,
     },
     email:{
         type: String,
+        required:true,
+        lowercase: true,
+        trim:true,
+        unique: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid email id"+ value);
+            }
+        }
     },
     password :{
-        typeof: String,
-    },
-    _id:{
         type: String,
+        required:true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Enter a strong password" + value);
+            }
+        }
     },
     age:{
         type : Number,
+        min: 18
+    },
+    gender:{
+        type: String,
+        validate(value){
+            if(!["male", "female","others"].includes(value)){
+                throw new Error("Gender data is not valid");
+            }
+        }
+    },
+    photoUrl:{
+        type:String,
+        default :"https://t4.ftcdn.net/jpg/02/44/43/69/360_F_244436923_vkMe10KKKiw5bjhZeRDT05moxWcPpdmb.jpg"
+    },
+    about:{
+        type: String,
+        default:"This is a default about of the user"
+    },
+    skills:{
+        type:[String],
     }
-});
+},{
+    timestamps: true
+}
+);
 
 module.exports = mongoose.model("User", userSchema);
